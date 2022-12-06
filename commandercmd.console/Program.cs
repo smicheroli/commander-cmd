fc90798c40843fa513154a8540fcc4aaa18aca69
smicheroli <58483574+smicheroli@users.noreply.github.com>
2022-12-06 15:09:33 +0100

    clean structure

--- commandercmd.console/Program.cs
+++ commandercmd.console/Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hey :O");
-Console.WriteLine("Copyright: Scrum Master: Loris Egli \nDebugger: Stefano");
+﻿namespace commandercmd.console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hey :O");
+            Console.WriteLine("Copyright: Scrum Master: Loris Egli \nDebugger: Stefano");
+        }
+    }
+}
