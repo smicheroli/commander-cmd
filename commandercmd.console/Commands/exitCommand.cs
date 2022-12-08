7490295385f722f800f33095de39259194e9649a
Bikerpema <101387831+Bikerpema@users.noreply.github.com>
2022-12-08 13:41:41 +0100

    test exitCommand

---
+++ commandercmd.console/Commands/exitCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandercmd.console.Commands
+{
+    public class testCommand : ShellCommand
+    {
+        public testCommand(string command, string parameter) : base(command, parameter)
+        {
+        }
+
+        public override void Execute()
+        {
+            Console.WriteLine(this.Parameter);
+        }
+    }
+}
