767500958399ddf4d5b0171ea3ba355e0f2f4fc5
smicheroli <58483574+smicheroli@users.noreply.github.com>
2022-12-13 13:49:16 +0100

    filesystem

---
+++ commandercmd.FileSystem/Drive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandercmd.console.FileSystem
+{
+    public class Drive
+    {
+        public List<FileSystemItem> Content { get; set; }
+
+    }
+}
