a48998b275c9a2fc13f8701eecaf3c9a4ffb7e9d
smicheroli <58483574+smicheroli@users.noreply.github.com>
2022-12-13 13:51:57 +0100

    namespace

--- commandercmd.FileSystem/File.cs
+++ commandercmd.FileSystem/File.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace commandercmd.console.FileSystem
+namespace commandercmd.FileSystem
 {
     public class File : FileSystemItem
     {
-        
+
     }
 }
