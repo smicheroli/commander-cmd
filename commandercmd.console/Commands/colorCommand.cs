a0c12b2ade08b54debceaf31e9d1ea8602b49f55
Noam Arnold <Noam.Arnold@bwz-rappi.ch>
2022-12-13 09:38:54 +0100

    Set color komplett umgeändert für mister sabi

--- commandercmd.console/Commands/colorCommand.cs
+++ commandercmd.console/Commands/colorCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,37 +17,122 @@ namespace commandercmd.console.Commands
         {
             string setcolor = this.Parameter;
             Console.WriteLine(setcolor);
-            // Check if the user provided two colors as arguments
-            if (setcolor.Length == 2)
+
+            //Split the color into 2 seperate words
+            string[] splitcolors = setcolor.Split(" ");
+
+            //Converting the Words to Uppercase
+            string[] systemcolors = splitcolors[0].ToUpper();
+            systemcolors[1] = splitcolors[1].ToUpper();
+
+            switch (systemcolors[0])
             {
-                string bcolorstring = setcolor.Substring(0, 1);
-                string tcolorstring = setcolor.Substring(1, 1);
-                // Try to parse the first argument as a ConsoleColor for the background coloryyy
-                ConsoleColor backgroundColor = ConsoleColor.Black;
-                if (Enum.TryParse(bcolorstring, out ConsoleColor parsedBackgroundColor))
-                {
-                    backgroundColor = parsedBackgroundColor;
-                }
-
-                // Try to parse the second argument as a ConsoleColor for the text color
-                ConsoleColor textColor = ConsoleColor.White;
-                if (Enum.TryParse(tcolorstring, out ConsoleColor parsedTextColor))
-                {
-                    textColor = parsedTextColor;
-                }
-
-                // Set the background and text colors of the console
-                Console.BackgroundColor = backgroundColor;
-                Console.ForegroundColor = textColor;
-
-                // Print a message to the console to confirm the colors have been set
-                Console.WriteLine("The background color has been set to " + backgroundColor + " and the text color has been set to " + textColor + ".");
+                case "BLAU":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "GELB":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "GRÜN" || "GRUEN":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "ROT":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "TÜRKIS" || "TUERKIS":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "LILA":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "HELLGRAU":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "GRAU":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "HELLBLAU":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "HELLGRÜN" || "HELLGRUEN":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "HELLTÜRKIS" || "HELLTUERKIS":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case "HELLROT":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "HELLLILA":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "HELLGELB":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "WEISS":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "SCHWARZ":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
             }
-            else
+            switch
+
+            switch (systemcolors[1])
             {
-                // Print an error message if the user did not provide two colors as arguments
-                Console.WriteLine("Please provide two colors as arguments, for example: color fa");
+                case "BLAU":
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "GELB":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "GRÜN" || "GRUEN":
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "ROT":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "TÜRKIS" || "TUERKIS":
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "LILA":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "HELLGRAU":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    break;
+                case "GRAU":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "HELLBLAU":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    break;
+                case "HELLGRÜN" || "HELLGRUEN":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    break;
+                case "HELLTÜRKIS" || "HELLTUERKIS":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    break;
+                case "HELLROT":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    break;
+                case "HELLLILA":
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    break;
+                case "HELLGELB":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    break;
+                case "WEISS":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    break;
+                case "SCHWARZ":
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
             }
+            switch
+
+            Console.WriteLine("The text color has been changed to " + systemcolors[0] + "and the background color has been changed to" + systemcolors[1] + ".");
+            Array.Clear(systemcolors);
         }
     }
 }
