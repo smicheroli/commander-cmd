0903ee0b6e3da6cbf86856d00bf0663afdde4a86
smicheroli <58483574+smicheroli@users.noreply.github.com>
2022-12-15 14:31:50 +0100

    filesystem

--- commandercmd.FileSystem/PersistenceService.cs
+++ commandercmd.FileSystem/PersistenceService.cs
@@ -10,29 +10,36 @@ namespace commandercmd.FileSystem
     public class PersistenceService
     {
         public String FilePath { get; set; }
-        private JsonSerializerSettings JsonSerializerSettings;
+        private JsonSerializerSettings jsonSerializerSettings;
 
         public PersistenceService(String filePath) {
             this.FilePath = filePath;
-            JsonSerializerSettings= new();
-            JsonSerializerSettings.TypeNameHandling= TypeNameHandling.All;
+            jsonSerializerSettings = new();
+            jsonSerializerSettings.TypeNameHandling= TypeNameHandling.All;
         }
 
         public void Save(IList<Drive> files)
         {
-            System.IO.File.WriteAllText(FilePath, JsonConvert.SerializeObject(files, JsonSerializerSettings));
+            System.IO.File.WriteAllText(FilePath, JsonConvert.SerializeObject(files, jsonSerializerSettings));
         }
 
         public IList<Drive> Load() {
             IList<Drive> files = new List<Drive>();
             if (System.IO.File.Exists(FilePath))
             {
-                files = JsonConvert.DeserializeObject<List<Drive>>(FilePath, JsonSerializerSettings);
-            }
-            if(files == null)
+                files = JsonConvert.DeserializeObject<List<Drive>>(FilePath, jsonSerializerSettings);
+            } else
             {
                 files= new List<Drive>();
-                files.Add(new Drive("C"));
+                Drive cDrive = new Drive('C');
+                Directory directory1 = new Directory("Pema");
+                directory1.Content.Add(new File("Loris.txt", "Das ist ein Test. Diktaturen verboten"));
+                cDrive.Content.Add(directory1);
+                Directory directory2 = new Directory("Pema1");
+                cDrive.Content.Add(directory2);
+                Directory directory3 = new Directory("Pema2");
+                cDrive.Content.Add(directory3);
+                files.Add(cDrive);
             }
             return files;
         }
